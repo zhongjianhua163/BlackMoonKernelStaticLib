def4d287d8c017ef539dc9d008254563ed32e508
钟建华 <171286005@qq.com>
2020-07-29 14:46:36 +0800

    修复 取星期几() 当日期在1899年12月30日之前时，计算错误的问题。

--- krnln/GetWeekDay.cpp
+++ krnln/GetWeekDay.cpp
@@ -5,8 +5,8 @@ INT GetWeekDay(DATE date)
 {
 	DATE dt,dd;
 	dt = modf(date,&dd);
-	INT nRet = (INT)fabs(fmod(dd,7));
-	if(nRet ==0)
-		nRet = 7;
+	INT nRet = (INT)fmod(dd,7);
+	if(nRet <= 0)
+		nRet += 7;
 	return nRet;
 }
